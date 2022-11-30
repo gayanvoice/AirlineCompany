d3d1d90c30b05d35048a331e0c293e99b51e2eac
Gayan Kuruppu <gayan.kuruppu@live.com>
2022-11-30 00:28:07 +0000

    Change models.py

--- AirlineCompany/settings.py
+++ AirlineCompany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#*yqw0_o%_7w0h7b3l%t62kyx%e#w-*=)348%%=7-)&nikks=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
